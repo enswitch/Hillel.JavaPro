6a94bcea9782f10018556f07480be95f0faa214a
Pavlo Parubchak <pavloparubchak@gmail.com>
2023-04-30 16:00:30 +0300

    hm7

---
+++ src/main/java/homeworks/hm7/GuessWord.java
@@ -0,0 +1,52 @@
+package homeworks.hm7;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessWord {
+    public void guessRandomWord(String[] words) {
+        Random random = new Random();
+        String secretWord = words[random.nextInt(words.length)];
+        boolean isGuessed = false;
+        System.out.println("System guessed the word, try to find it out! Use lowercase only!");
+
+        char[] hashes = new char[15];
+        for (int i = 0; i < hashes.length; i++) {
+            hashes[i] = '#';
+        }
+
+        while (!isGuessed) {
+            Scanner scan = new Scanner(System.in);
+            String userInput = scan.nextLine();
+            for (int i = 0; i < userInput.length(); i++) {
+                char c = userInput.charAt(i);
+                if (c >= 'A' && c <= 'Z') {
+                    System.out.println("Please, do not use Uppercase letters!");
+                }
+            }
+
+            if (userInput.equals(secretWord)) {
+                System.out.println("Congratulations! You guessed the word!");
+                isGuessed = true;
+            } else {
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (i < userInput.length() && userInput.charAt(i) == secretWord.charAt(i)) {
+                        hashes[i] = secretWord.charAt(i);
+                    }
+                }
+                System.out.println(new String(hashes));
+                System.out.println("Incorrectly! Try again");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GuessWord player = new GuessWord();
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak",
+                "kiwi", "mango", "mushroom", "nut", "olive", " pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+
+        player.guessRandomWord(words);
+    }
+}
---
+++ src/main/java/homeworks/hm7/StringOperations.java
@@ -0,0 +1,47 @@
+package homeworks.hm7;
+
+public class StringOperations {
+
+    public int findSymbolOccurance(String str, char c1) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c2 = str.charAt(i);
+            if (c2 == c1) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int findWordPosition(String source, String target) {
+        if (source.contains(target)) {
+            return source.indexOf(target);
+        } else {
+            return -1;
+        }
+    }
+
+    public String stringReverse(String str) {
+        StringBuilder strb = new StringBuilder();  // I did it with loop, but I can do it with "return new StringBuilder(str).reverse.toString()"
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            strb.append(c);
+        }
+        return String.valueOf(strb);
+    }
+
+    public boolean isPalindrome(String str) {
+        String reverseString = new StringBuilder(str).reverse().toString();
+        return str.equals(reverseString);
+    }
+
+
+    public static void main(String[] args) {
+        StringOperations str = new StringOperations();
+        System.out.println("Char repeated " + str.findSymbolOccurance("Hillel hillel", 'l') + " times in a string");
+        System.out.println("Target in source starts at index: " + str.findWordPosition("Hillel", "lel"));
+        System.out.println("Reverse string: " + str.stringReverse("Hillel hillel"));
+        System.out.println("String is a palindrome: " + str.isPalindrome("repaper"));
+
+    }
+}
