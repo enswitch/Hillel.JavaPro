6891e365a848e7f2aa866807a6bfbdd7688e1435
Pavlo Parubchak <pavloparubchak@gmail.com>
2023-05-06 14:11:21 +0300

    hm8 update

--- src/main/java/homeworks/hm8/ArrayDataException.java
+++ src/main/java/homeworks/hm8/ArrayDataException.java
@@ -1,6 +1,6 @@
 package homeworks.hm8;
 
-public class ArrayDataException extends RuntimeException {
+public class ArrayDataException extends Exception {
     public ArrayDataException(String message) {
         super(message);
     }
--- src/main/java/homeworks/hm8/ArraySizeException.java
+++ src/main/java/homeworks/hm8/ArraySizeException.java
@@ -1,6 +1,6 @@
 package homeworks.hm8;
 
-public class ArraySizeException extends RuntimeException {
+public class ArraySizeException extends Exception {
     public ArraySizeException(String message) {
         super(message);
     }
